8c2fa593b12b52750622a2e5b318edce77e4aa4b
rachmatrisky <rachmatrisky5@gmail.com>
2025-05-24 18:56:24 +0800

    second commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = joblib.load("spam_classifier_model.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
+
+class EmailRequest(BaseModel):
+    message: str
+
+@app.post("/predict")
+async def predict(data: EmailRequest):
+    vector = vectorizer.transform([data.message])
+    prediction = model.predict(vector)[0]
+    proba = model.predict_proba(vector)[0]
+    spam_prob = round(proba[1] * 100, 2)
+    ham_prob = round(proba[0] * 100, 2)
+    return {
+        "prediction": prediction,
+        "spam_probability": spam_prob,
+        "ham_probability": ham_prob,
+        }
